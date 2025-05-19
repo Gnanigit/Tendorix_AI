2091228549060b535e15b06d011beed2fe76cf2a
dev1_clofast <gnani4412@gmail.com>
2025-05-19 15:39:44 +0530

    updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,12 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["fffa-2409-40f0-d9-7ba7-758e-4bdd-ca9c-d5bd.ngrok-free.app"], // Add this line
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
